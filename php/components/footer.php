98ab5377c729f638bfffd4807f46a11b90ca7c33
dimitribenhamma <168719459+dimitribenhamma@users.noreply.github.com>
2024-07-31 15:14:18 +0200

    Update classe Telephone footer.php

--- php/components/footer.php
+++ php/components/footer.php
@@ -1,2 +1,14 @@
 <footer class="footer">
+<?php 
+class Telephone {
+	var $telephone;
+	function __construct($telephone){
+		$this->telephone = $telephone;
+	}
+	
+	function getTelephone(){
+			return $this->telephone;
+	}
+}
+?>
 	<div style="font-size:18px;margin-top:10px">+33 6 31 85 79 07 &nbsp; &nbsp;<img src="../img/france.png" width="30px" height="20px" style="margin-top:-10px;" /></div><div style="margin-top:5px">Dimitri BENHAMMA</div><div><span style="margin-top:5px;float:right;padding-right:35px;">2024<span style="display:block;font-size:8px">ARGYROS</span></span><img src="../img/logo studi.png" width="140px" height="40px" style="padding-bottom:5px;float:right;padding-right:10px;" /></div></footer>

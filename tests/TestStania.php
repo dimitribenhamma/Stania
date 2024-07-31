049e535548d14e05a235d442e0cb558c136a5ea3
dimitribenhamma <168719459+dimitribenhamma@users.noreply.github.com>
2024-07-31 17:36:24 +0200

    Add files via upload

---
+++ tests/TestStania.php
@@ -0,0 +1,22 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once 'C:\ProgramData\Jenkins\.jenkins\workspace\MonPremierJob\php\components\footer.php';
+
+class TestStack extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+		$number = new Telephone();
+        $this->assertEquals("+33 6 31 85 79 07", $number->getTelephone());
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+}
+?>
